b41e563eeca7bbf4fad50d2478ecd4e11a4525a4
Solvexis_LDK <gojda7693@gmail.com>
2026-07-18 19:55:33 +0300

    Create Idk.lua

---
+++ Idk.lua
@@ -0,0 +1,7 @@
+task.spawn(function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/D83SxixS"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://pastefy.app/3uvStd1e/raw"))()
+end)
